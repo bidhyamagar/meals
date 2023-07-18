7a55c6ddc90a5ff9dcae8cce38c38c255777415f
bidhyamagar <bidhyamagar042@gmail.com>
2023-07-18 04:29:48 -0700

    website completed

---
+++ layouts/deafult.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <header>
+            <navbar-components/>
+        </header>
+
+        <main>
+            <slot></slot>
+        </main>
+        
+        <footer>
+            <footer-component></footer-component>
+        </footer>
+    </div>
+    
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
